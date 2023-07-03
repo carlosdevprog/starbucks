36be131b1c1594c248959d44d97fd37d50fa7e15
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2023-07-03 18:18:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let img = document.querySelector('.starbucks')
+
+let circle = document.querySelector('.circulo')
+
+
+
+
+
+const changeImg = (address) => {
+    img.src = address
+}
+
+const changeColor = (color) => {
+    circle.style.backgroundColor = color
+}
